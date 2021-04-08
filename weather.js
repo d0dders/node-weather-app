751fad89ef2e7acac39f456711c27fded776ae50
d0dders <dodders@outlook.com>
2021-04-08 23:20:47 +0100

    Added error handling

--- weather.js
+++ weather.js
@@ -8,20 +8,37 @@ function printWeather(weather){
     console.log(`The temperature in ${location} is ${temp}`);
 }
 
+function printError(error){
+    console.error(error.message);
+}
+
 
 function get(location) {
-    const request = http.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${settings.apiKey}`, (res) => {
-        if (res.statusCode === 200) {
-            let responseString = "";
-            res.on('data', (data) => {
-                responseString += data.toString()
-            });
-            res.on('end', () => {
-                const requestJson = JSON.parse(responseString);
-                printWeather(requestJson);
-            });
-        }
-    });
+    try{
+        const request = http.get(`http://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${settings.apiKey}`, (res) => {
+            if (res.statusCode === 200) {
+                let responseString = "";
+                res.on('data', (data) => {
+                    responseString += data.toString()
+                });
+                res.on('end', () => {
+                    try{
+                        const requestJson = JSON.parse(responseString);
+                        printWeather(requestJson);
+                    } catch(error){
+                        printError(error);
+                    }
+                    
+                });
+            } else {
+                const statusError = new Error(`Request for ${location} caused a ${http.STATUS_CODES[res.statusCode]} error`);
+                printError(statusError);
+            }
+        });
+    } catch(error){
+        printError(error);
+    }
+    
 }
 
 module.exports.get = get;
